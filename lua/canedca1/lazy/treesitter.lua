2ca0e4e211b050ec12b5151f903c00e5b788bc44
Carter Canedy <canedca1@novartis.net>
2024-08-07 09:00:19 -0700

    fix treesitter compiler settings

--- lua/canedca1/lazy/treesitter.lua
+++ lua/canedca1/lazy/treesitter.lua
@@ -1,31 +1,34 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = {
-      "vimdoc",
-      "javascript",
-      "typescript",
-      "c",
-      "lua",
-      "rust",
-      "jsdoc",
-      "bash",
-      "make",
-      "cmake",
-      "cpp"
-    },
+  config = function()
+    require("nvim-treesitter.install").compilers = { "zig" }
 
-    sync_install = false,
-    auto_install = true,
-    indent = {
-      enable = true
-    },
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "c",
+        "lua",
+        "rust",
+        "jsdoc",
+        "bash",
+        "make",
+        "cmake",
+        "cpp"
+      },
 
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { "markdown" }
-    }
-  },
+      sync_install = false,
+      auto_install = true,
+      indent = {
+        enable = true
+      },
+
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "markdown" }
+      }
+    })
+  end,
 }

ff49860f069ea2f0489ed03e73252ce5cd849a71
Carter Canedy <cartercanedy42@gmail.com>
2024-09-06 09:24:07 -0700

    fix formatting

--- lua/canedca1/lazy/lualine.lua
+++ lua/canedca1/lazy/lualine.lua
@@ -1,33 +1,47 @@
 return {
   "cartercanedy/lualine.nvim",
+
   opts = {
     options = {
-      icons_enabled = true,
-      theme = "tomorrow_night",
-      component_separators = { left = '', right = ''},
-      section_separators = { left = '', right = ''},
+      component_separators = {
+        left = '',
+        right = ''
+      },
+
+      section_separators = {
+        left = '',
+        right = ''
+      },
+
       disabled_filetypes = {
         statusline = {},
         winbar = {},
       },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = true,
+
       refresh = {
         statusline = 1000,
         tabline = 1000,
         winbar = 1000,
-      }
+      },
+
+      icons_enabled = true,
+      theme = "tomorrow_night",
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = true
     },
+
     sections = {
       lualine_a = {
         'mode'
       },
+
       lualine_b = {
         'branch',
         'diff',
         'diagnostics',
       },
+
       lualine_c = {
         'filename',
         {
@@ -38,30 +52,38 @@ return {
           },
         },
       },
+
       lualine_x = {
         'encoding',
         'fileformat',
         'filetype'
       },
+
       lualine_y = {
         'progress'
       },
+
       lualine_z = {
         'location'
       },
     },
+
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
+
       lualine_c = {
         'filename'
       },
+
       lualine_x = {
         'location'
       },
+
       lualine_y = {},
       lualine_z = {}
     },
+
     tabline = {},
     winbar = {},
     inactive_winbar = {},

157a0faad46db34adb333cef00f17aa94f9ed4ae
Carter Canedy <cartercanedy42@gmail.com>
2024-11-22 13:42:36 -0800

    auto-update buffer on fs file change

--- lua/canedca1/set.lua
+++ lua/canedca1/set.lua
@@ -1,3 +1,10 @@
+vim.o.autoread = true
+
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
 vim.opt.scrolloff = 8
 vim.opt.swapfile = false
 vim.opt.backup = false

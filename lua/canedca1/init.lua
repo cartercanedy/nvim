6a6ef31edd1c10ef136fa3bee963a32a506ace0b
Carter Canedy <cartercanedy42@gmail.com>
2024-11-26 10:49:53 -0800

    ignore weird errors being emitted from rust-analyzer

--- lua/canedca1/init.lua
+++ lua/canedca1/init.lua
@@ -51,7 +51,7 @@ autocmd("FileType", {
 for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
     local default_diagnostic_handler = vim.lsp.handlers[method]
     vim.lsp.handlers[method] = function(err, result, context, config)
-        if err ~= nil and err.code == -32802 then
+        if err ~= nil and err.code < 0 then
             return
         end
         return default_diagnostic_handler(err, result, context, config)

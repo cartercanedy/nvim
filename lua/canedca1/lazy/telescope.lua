0f98eebb1bb703e6b2811a84245e728330bf84dc
Carter Canedy <cartercanedy42@gmail.com>
2025-07-29 06:26:27 -0700

    use telescope for buffer management

--- lua/canedca1/lazy/telescope.lua
+++ lua/canedca1/lazy/telescope.lua
@@ -1,3 +1,7 @@
+local opts = {
+  noremap = true,
+  silent = true
+}
 return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.5",
@@ -11,9 +15,10 @@ return {
   opts = {},
   init = function()
     local telescope = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>pf', telescope.find_files, {})
-    vim.keymap.set('n', '<C-p>', telescope.git_files, {})
-    vim.keymap.set('n', '<leader>ps', function() telescope.live_grep() end)
-    vim.keymap.set('n', '<leader>vh', telescope.help_tags, {})
+    vim.keymap.set('n', '<leader>pf', telescope.find_files, opts)
+    vim.keymap.set('n', '<C-p>', telescope.git_files, opts)
+    vim.keymap.set('n', '<leader>ps', telescope.live_grep, opts)
+    vim.keymap.set('n', '<leader>vh', telescope.help_tags, opts)
+    vim.keymap.set( "n", "<leader>bv", telescope.buffers, opts)
   end
 }

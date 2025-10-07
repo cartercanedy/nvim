a21e0853d3544058e9286059f9f2b9e863e83cd4
Carter Canedy <carter.canedy@novartis.com>
2025-10-06 19:20:57 -0700

    angle bracket autopair

--- lua/canedca1/lazy/autopairs.lua
+++ lua/canedca1/lazy/autopairs.lua
@@ -1,5 +1,30 @@
 return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
-  opts = {}
+  config = function()
+    local npairs = require("nvim-autopairs")
+    local rule = require("nvim-autopairs.rule")
+    local cond = require("nvim-autopairs.conds")
+
+    npairs.setup({})
+
+    npairs.add_rule(rule('<', '>', {
+      -- if you use nvim-ts-autotag, you may want to exclude these filetypes from this rule
+      -- so that it doesn't conflict with nvim-ts-autotag
+      '-html',
+      '-javascriptreact',
+      '-typescriptreact',
+    }):with_pair(
+        -- regex will make it so that it will auto-pair on
+        -- `a<` but not `a <`
+        -- The `:?:?` part makes it also
+        -- work on Rust generics like `some_func::<T>()`
+        cond.before_regex('%a+:?:?$', 3)
+      ):with_move(function(opts)
+        return opts.char == '>'
+      end)
+    )
+
+    npairs.add_rule(rule("<", ">", { }):with_pair(cond.before_regex("template $", 9)))
+  end
 }
